a0242f01a53f6c29a1714bd2cc46c4353c1782d7
Axora <tech40535@gmail.com>
2025-10-20 16:32:42 +0530

    Delete src directory

--- src/core/models/Slide.ts
+++
@@ -1,9 +0,0 @@
-export default class Slide {
-  constructor(
-    public id: string,
-    public title: string,
-    public highlighted: string | null,
-    public subtitle: string,
-    public image: number
-  ) {}
-}
